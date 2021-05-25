c1053a93052e094bc98a0f16dfa24859db54c3ee
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-05-25 05:48:34 +0000

    [Fix]check

--- app/controllers/users/checks_controller.rb
+++ app/controllers/users/checks_controller.rb
@@ -1,15 +1,15 @@
-class Users::ChecksController < 
+class Users::ChecksController < ApplicationController
   
   def create
-    @information = Information.find(params[:information_id])
+    @item = Item.find(params[:item_id])
     # confirmation.user_id = current_user.idが済んだ状態で生成
-    check = current_user.checks.build(information_id: params[:information_id])
+    check = current_user.checks.build(item_id: params[:item_id])
     check.save
   end
 
   def destroy
-    @information = Information.find(params[:information_id])
-    check = current_user.checks.find_by(information_id: params[:information_id])
+    @item = Item.find(params[:item_id])
+    check = current_user.checks.find_by(item_id: params[:item_id])
     check.destroy
   end
   
